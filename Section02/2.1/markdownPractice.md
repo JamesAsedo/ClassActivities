394c65e45edfe7845598617b68fb6daabe730263
James Asedo <james.asedo@gmail.com>
2023-01-17 19:39:02 -0800

    Section 2 ClassActivities

---
+++ Section02/2.1/markdownPractice.md
@@ -0,0 +1,17 @@
+### Heading One
+
+## Heading Two
+
+# Heading Three
+
+_Grocery List_
+- Apples
+- Bananas
+- Oranges
+
+**Primary Colors**
+1. Blue
+2. Red
+3. Yellow
+
+~~Strikethrough~~
---
+++ Section02/2.2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,32 @@
+// create a variable called "name" that references a string
+let name = "James";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "Rock Bottom";
+// create a variable called "wage" that references a number
+let wage = "60k";
+// create a variable called "age" that references a number
+let age = 41;
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = false;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = "Football";
+console.log(favoriteThing);
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = "Summer Nights";
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = 23;
+// Happy Birthday!
+// Reassign "age" to a new value
+age = 42;
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage = "70k";
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = true;
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
